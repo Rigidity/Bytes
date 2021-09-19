6c908d95919348a1281fbbecd82e28dbc2a64b27
Rigidity <me@rigidnetwork.com>
2021-09-19 00:27:21 -0400

    Initial byte helpers

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Bytes
 Utilities for manipulating bytes, written in TypeScript.
+
+## Installation
+`npm install @rigidity/bytes`
+
+## Types
+
+### Endianness
+Represents either `"little"` or `"big"` endian.
+
+### Encoding
+An exported alias for `BufferEncoding` for completeness.
+
+## Functions
+
+### Utilities
+`concatBytes(...list: Buffer[]): Buffer`
+
+### String
+`bytesToString(bytes: Buffer, encoding: Encoding): string`
+`stringToBytes(text: string, encoding: Encoding): Buffer`
+
+### Hex
+`bytesToHex(bytes: Buffer): string`
+`hexToBytes(hex: string): Buffer`
+
+### BigInt
+`bytesToInt(bytes: Buffer, endianness: Endianness): bigint`
+`intToBytes(int: bigint, size: number, endianness: Endianness): Buffer`
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { toBigIntBE, toBigIntLE, toBufferBE, toBufferLE } from 'bigint-buffer';
+
+export type Endianness = 'little' | 'big';
+export type Encoding = BufferEncoding;
+
+export function bytesToString(bytes: Buffer, encoding: Encoding): string {
+    return bytes.toString(encoding);
+}
+
+export function stringToBytes(text: string, encoding: Encoding): Buffer {
+    return Buffer.from(text, encoding);
+}
+
+export function bytesToHex(bytes: Buffer): string {
+    return bytes.toString('hex');
+}
+
+export function hexToBytes(hex: string): Buffer {
+    return Buffer.from(hex, 'hex');
+}
+
+export function bytesToInt(bytes: Buffer, endianness: Endianness): bigint {
+    return endianness === 'little' ? toBigIntLE(bytes) : toBigIntBE(bytes);
+}
+
+export function intToBytes(
+    int: bigint,
+    size: number,
+    endianness: Endianness
+): Buffer {
+    return endianness === 'little'
+        ? toBufferLE(int, size)
+        : toBufferBE(int, size);
+}
+
+export function concatBytes(...list: Buffer[]): Buffer {
+    let length = 0;
+    for (const bytes of list) length += bytes.length;
+    const result = Buffer.alloc(length);
+    let index = 0;
+    for (const bytes of list) {
+        result.set(bytes, index);
+        index += bytes.length;
+    }
+    return result;
+}
